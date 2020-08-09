09eef66a6540c28bf358c5f928eb42f7ae91b254
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-09 19:39:34 -0400

    primitive types: int

---
+++ exercise-files/ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+// package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value  = " + myMinIntValue);
+        System.out.println("Integer Maximum Value  = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1)); // overflow
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1)); // overflow
+
+        int myMaxIntTest = 2147483648; // Integer number too large
+        int myLegibleInt = 2_147_483_647;
+     
+    }
+}
