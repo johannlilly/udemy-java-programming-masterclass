d4810b695f27731f3284ccc65c246e295d350a40
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 10:27:17 -0400

    methods, concise

---
+++ exercise-files/Methods/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+  
+  public static void main (String[] args) {
+
+    boolean gameOver = true;
+    int score = 800;
+    int levelCompleted = 5;
+    int bonus = 100;
+
+    calculateScore(
+      gameOver,
+      score,
+      levelCompleted,
+      bonus
+    );
+
+    calculateScore(
+      true,
+      10_000,
+      8,
+      200
+    );
+
+  }
+
+  public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+    if (gameOver) {
+      int finalScore = score + (levelCompleted * bonus);
+      finalScore += 2_000;
+      System.out.println("Your final score was " + finalScore);
+    }
+
+  }
+
+}
