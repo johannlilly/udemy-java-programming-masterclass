cc9f0338bdce025d3a27ae77d2dc4cb7d50b407e
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 13:25:01 -0400

    coding exercise 3: barking dog solution

---
+++ exercise-files/BarkingDog/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    shouldWakeUp(true, 1);
+    shouldWakeUp(false, 2);
+    shouldWakeUp(true, 8);
+    shouldWakeUp(true, -1);
+  }
+  public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+    boolean wakeUp = 
+      0 <= hourOfDay && hourOfDay < 24
+        ? barking
+          ? hourOfDay < 8 || 23 == hourOfDay
+            ? true  // dog barking, right hour
+            : false // dog barking, wrong hour
+          : false // valid input, dog not barking
+        : false // invalid input
+      ;
+    return wakeUp;
+  }
+}
