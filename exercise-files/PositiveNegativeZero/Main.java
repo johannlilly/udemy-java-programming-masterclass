f763ff02fcd9f74472234d17865d4a2a0f601de1
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 12:13:58 -0400

    using udemy exercise

---
+++ exercise-files/PositiveNegativeZero/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  
+  public static void main(String[] args) {
+
+
+
+  }
+
+  public static void checkNumber(int number) {
+
+    String check = 
+      number > 0 ? "positive" :
+      number < 0 ? "negative" :
+      "zero";
+
+    System.out.println(check);
+
+  }
+
+}
