67d59aeda77ea9b4a0b94f49ed80f1b92d5fa508
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 06:49:55 -0400

    logical and operator

--- exercise-files/Operators/src/Main.java
+++ exercise-files/Operators/src/Main.java
@@ -48,6 +48,16 @@ public class Main {
             System.out.println("And I am scared of aliens");
         }
 
+        int topScore = 100;
+        if (topScore != 100) { // ==, !=, >, <. >=, <=
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) { // ||
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 
 }
