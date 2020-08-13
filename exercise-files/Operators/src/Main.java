60af02984cbd7800130aa55851a9fb4c82988c11
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 07:10:18 -0400

    assignment vs equals operator

--- exercise-files/Operators/src/Main.java
+++ exercise-files/Operators/src/Main.java
@@ -63,6 +63,15 @@ public class Main {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        boolean isCar = false;
+        if (isCar) { // isCar == true
+            System.out.println("This is not supposed to happen");
+        }
     }
 
 }
