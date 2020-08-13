78edd18de48ad7bb697f3416a5fd94ed8d226781
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 15:59:46 -0400

    coding exercise - equal sum checker solution

---
+++ exercise-files/EqualSumChecker/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+
+  }
+  public static boolean hasEqualSum(int number0, int number1, int number10) {
+    return number0 + number1 == number10;
+  }
+}
