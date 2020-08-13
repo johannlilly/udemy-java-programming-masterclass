d04695776e40bd0a226c7f526d5752c5f14b2e27
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-13 15:57:17 -0400

    coding exercise decimalcomparator solution

---
+++ exercise-files/DecimalComparator/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+
+  }
+  public static boolean areEqualByThreeDecimalPlaces(double number0, double number1) {
+    return (long) (number0 * 1000) == (long) (number1 * 1000);
+  }
+}
