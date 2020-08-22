0bfa6676bcf2ff1b2e8a07d09bc4a939cc804432
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 17:09:48 -0400

    for loop challenge, decrement

--- exercise-files/ForLoop/CalculateInterest.java
+++ exercise-files/ForLoop/CalculateInterest.java
@@ -1,6 +1,6 @@
 public class CalculateInterest {
   public static void main(String[] args) {
-    for (int i = 2; i <= 8; i++) {
+    for (int i = 8; i >= 2; i--) {
       System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10_000d, (double) i)));
     }
   }
@@ -8,4 +8,5 @@ public class CalculateInterest {
   public static double calculateInterest(double amount, double interestRate) {
     return (amount * (interestRate / 100d));
   }
+
 }
