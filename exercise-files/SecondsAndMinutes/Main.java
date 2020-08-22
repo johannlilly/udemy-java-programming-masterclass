04e97d65a8c19ec02b97ecacab8ec30f038fd61e
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 10:14:58 -0400

    assign an immutable variable
    Do this using the `final` keyword

--- exercise-files/SecondsAndMinutes/Main.java
+++ exercise-files/SecondsAndMinutes/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+  private static final String INVALID_VALUE_MESSAGE = "Invalid value";
   public static void main(String[] args) {
 
     System.out.println(getDurationString(128));
@@ -10,9 +11,9 @@ public class Main {
   public static String getDurationString(int minutes, int seconds) {
 
     return !(minutes >= 0)
-    ? "Invalid value"
+    ? INVALID_VALUE_MESSAGE
     : !(minutes >= 0 && seconds >= 0 && seconds < 60)
-      ? "Invalid value"
+      ? INVALID_VALUE_MESSAGE
       // XXh YYm ZZs
       : minutes >= 0 && minutes < 10
         ? (int) Math.floor(minutes / 60) + "h 0" + minutes % 60 + "m " + seconds + "s"
@@ -24,7 +25,7 @@ public class Main {
 
     return seconds >= 0
     ? getDurationString((int) Math.floor(seconds / 60) , seconds % 60)
-    : "Invalid value";
+    : INVALID_VALUE_MESSAGE;
 
   }
 
