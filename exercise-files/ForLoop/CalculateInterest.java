66f6dd7617630509c81a19d6345b9a348a6fdd93
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 16:57:28 -0400

    introduction to for loops

---
+++ exercise-files/ForLoop/CalculateInterest.java
@@ -0,0 +1,13 @@
+public class CalculateInterest {
+  public static void main(String[] args) {
+    System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Loop " + i + " hello!");
+    }
+  }
+  
+  public static double calculateInterest(double amount, double interestRate) {
+    return (amount * (interestRate / 100));
+  }
+}
