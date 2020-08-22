412d91eab64bab15b7e1491c0686ac5abfc29f09
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 17:10:20 -0400

    Algorithm to check for prime numbers

--- exercise-files/ForLoop/CalculateInterest.java
+++ exercise-files/ForLoop/CalculateInterest.java
@@ -9,4 +9,19 @@ public class CalculateInterest {
     return (amount * (interestRate / 100d));
   }
 
+  public static boolean isPrime(int n) {
+
+    if (n == 1)  {
+      return false;
+    }
+
+    for (int i=2; i <= n/2; i++) {
+      if (n % 1 == 0) {
+        return false;
+      }
+    }
+
+    return true;
+    
+  }
 }
