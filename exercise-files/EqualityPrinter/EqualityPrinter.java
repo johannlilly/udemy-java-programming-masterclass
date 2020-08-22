0650d38d1b10e212da0b3e40bb025d9f3242e010
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 10:27:26 -0400

    Equality printer challenge
    + a little extra practice using `private static final String` for an error message

---
+++ exercise-files/EqualityPrinter/EqualityPrinter.java
@@ -0,0 +1,17 @@
+public class IntEqualityPrinter {
+  private static final String ERROR_MESSAGE = "Invalid Value";
+  private static final String EQUALITY_MESSAGE = "All numbers are equal";
+  private static final String INEQUALITY_MESSAGE = "All numbers are different";
+  private static final String SEMI_EQUALITY_MESSAGE = "Neither all are equal or different";
+  public static void printEqual(int a, int b, int c) {
+    String equalityResult =  a < 0 || b < 0 || c < 0
+    ? ERROR_MESSAGE
+    : a == b && b == c
+      ? EQUALITY_MESSAGE
+      : a == b || b == c
+        ? SEMI_EQUALITY_MESSAGE
+        : INEQUALITY_MESSAGE;
+    
+    System.out.println(equalityResult);
+  }
+}
