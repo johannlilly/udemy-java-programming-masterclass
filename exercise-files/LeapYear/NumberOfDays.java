47b4e937edddd6c62f1d7e31abfee7526fcfda99
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 16:34:36 -0400

    not as concise but may be more legible

--- exercise-files/LeapYear/NumberOfDays.java
+++ exercise-files/LeapYear/NumberOfDays.java
@@ -12,11 +12,22 @@ public class NumberOfDaysInMonth {
     ;
   }
   public static int getDaysInMonth(int month, int year) {
-    return month < 1 || month > 12 || year < 1 || year >= 10_000
+    return 
+         month < 1 || month > 12
+      || year < 1  || year >= 10_000
     ?  -1
-    : month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
+    :    month == 1 
+      || month == 3
+      || month == 5
+      || month == 7
+      || month == 8
+      || month == 10
+      || month == 12
       ? 31
-      : month == 4 || month == 6 || month == 9 || month == 11
+      :  month == 4
+      || month == 6
+      || month == 9
+      || month == 11
         ? 30
         : month == 2 && isLeapYear(year)
           ? 29
