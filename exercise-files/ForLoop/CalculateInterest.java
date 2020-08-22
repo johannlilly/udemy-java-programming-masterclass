bc381230d9b51ca1ec409d12f34aa8b005e2d11b
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 17:05:40 -0400

    for loops challenge
    with string formatting

--- exercise-files/ForLoop/CalculateInterest.java
+++ exercise-files/ForLoop/CalculateInterest.java
@@ -1,13 +1,11 @@
 public class CalculateInterest {
   public static void main(String[] args) {
-    System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
-
-    for (int i = 0; i < 5; i++) {
-      System.out.println("Loop " + i + " hello!");
+    for (int i = 2; i <= 8; i++) {
+      System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10_000d, (double) i)));
     }
   }
   
   public static double calculateInterest(double amount, double interestRate) {
-    return (amount * (interestRate / 100));
+    return (amount * (interestRate / 100d));
   }
 }
