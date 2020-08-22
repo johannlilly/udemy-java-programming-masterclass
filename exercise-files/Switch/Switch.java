93018bb4b19e241ad93aa5b9ebc7ca502830a0af
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-08-22 11:16:09 -0400

    control flow statements - switch

---
+++ exercise-files/Switch/Switch.java
@@ -0,0 +1,34 @@
+public class Switch {
+    public static void main(String[] args) {
+      int value = 1;
+      if (value == 1) {
+        System.out.println("Value was 1");
+      } else if (value == 2) {
+        System.out.println("Value was 2");
+      } else {
+        System.out.println("Value was not 1 or 2");
+      }
+
+      int switchValue = 3;
+
+      switch(switchValue) {
+        case 1:
+          System.out.println("Value was 1 ");
+          break;
+
+        case 2:
+          System.out.println("Value was 2");
+          break;
+
+        case 3:case 4:case 5:
+          System.out.println("Value was 3, 4, or 5");
+          break;
+
+        default:
+          System.out.println("Value was not 1 or 2");
+          break;
+      }
+
+      // more code here, where program continues after {break}
+    }
+}
